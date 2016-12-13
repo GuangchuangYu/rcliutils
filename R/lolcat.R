9822e361ba4ce145f6dd840a8f641214f3ee962f
guangchuang yu <guangchuangyu@gamil.com>
2016-12-13 19:59:55 +0800

    lolcat

--- R/lolcat.R
+++ R/lolcat.R
@@ -7,5 +7,28 @@
 ##' @return characters
 ##' @author guangchuang yu
 lolcat <- function(msg) {
-    system(paste0("echo '",paste(msg, collapse="\n"), "'|lolcat"))
+    res <- system(paste0("echo '",paste(msg, collapse="\n"), "'|lolcat"), intern=TRUE)
+    res <- paste(res, collapse="\n")
+    class(res) <- "lolcat"
+    return(res)
 }
+
+
+
+##' @method print lolcat
+##' @export
+print.lolcat <- function(x, ...) {
+    cat(x, "\n")
+}
+
+##' @importFrom methods setOldClass
+setOldClass("lolcat")
+
+##' show method for lolcat instance
+##'
+##' @name show
+##' @docType methods
+##' @rdname show-methods
+##' @aliases lolcat-class show,lolcat-method
+##' @exportMethod show
+setMethod("show", signature(object="lolcat"), function(object) print.lolcat(object))
